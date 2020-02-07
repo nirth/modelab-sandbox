59baec6b1ed4a909a159428aae68cd794005198f
David Sergey <me@davidsergey.com>
2020-02-07 11:57:56 +0000

    Extracting Tx and Scenario Logic to SDK sub-package

---
+++ sandbox-ui/src/sbdk/datamodel.ts
@@ -0,0 +1,47 @@
+export type HealthStatus = {
+  serviceName: string
+  serviceId: string
+  ok: boolean
+  status?: string
+  sidecars?: HealthStatus[]
+}
+
+export enum TxType {
+  DirectDebitPayment = 'DIRECT_DEBIT_PAYMENT',
+  DirectDebitAnnouncement = 'DIRECT_DEBIT_ANNOUNCEMENT',
+  CreditTransfer = 'CREDIT_TRANSFER',
+}
+
+export type Tx = {
+  type: TxType
+  datetime: string
+  amount: string
+  creditorCustomer: string
+  creditorBankAccount: string
+  sender: string
+  receiver: string
+  debitorCustomer: string
+  debitorBankAccount: string
+}
+
+export type DirectDebitPaymentTx = Tx & {
+  type: TxType.DirectDebitPayment
+}
+
+export type DirectDebitAnnouncementTx = Tx & {
+  type: TxType.DirectDebitAnnouncement
+}
+
+export type CreditTransferTx = Tx & {
+  type: TxType.CreditTransfer
+}
+
+export type Scenario = {
+  id: string
+  slug: string
+  title: string
+  description: string
+  txs: Tx[]
+}
+
+export type Scenarios = Scenario[]
---
+++ sandbox-ui/src/sbdk/index.ts
@@ -0,0 +1,5 @@
+export {
+  createCreditTransferTx,
+  createDirectDebitAnnouncementTx,
+  createDirectDebitPaymentTx,
+} from './txs'
