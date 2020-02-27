a7bf47bd2dc5e7f66e67abd2245de0db9405f0ac
David Sergey <me@davidsergey.com>
2020-02-27 08:53:58 +0000

    WIP

--- sandbox-ui/src/assets/payment-scenarios/index.ts
+++ sandbox-ui/src/assets/payment-scenarios/index.ts
@@ -9,24 +9,24 @@ import { utilityBills } from './utility-bills'
 import { coupleOfLargeDds } from './couple-of-large-dds'
 
 export const scenarios: Scenarios = [
-  // coupleOfLargeDds,
-  // stableSalary,
+  coupleOfLargeDds,
+  stableSalary,
   composeScenarios(
     'Stable Salary and Utilities',
     'stable-salary-and-utilities',
     'Alice has a good job, and pays her bills!',
     [stableSalary, utilityBills, coupleOfLargeDds]
   ),
-  // composeScenarios(
-  //   'Stable Salary and Occasional Coffee',
-  //   'stable-salary-and-occasional-coffee',
-  //   'Alice has a good job, and sometimes buys a cup of coffee!',
-  //   [stableSalary, occasionalCoffee]
-  // ),
-  // composeScenarios(
-  //   'Coffee Addict with Stable Salary',
-  //   'coffee-addict-with-stable-salary',
-  //   'Alice has a good job, and even healthier coffee addiction!',
-  //   [stableSalary, lotsOfCoffees]
-  // ),
+  composeScenarios(
+    'Stable Salary and Occasional Coffee',
+    'stable-salary-and-occasional-coffee',
+    'Alice has a good job, and sometimes buys a cup of coffee!',
+    [stableSalary, occasionalCoffee]
+  ),
+  composeScenarios(
+    'Coffee Addict with Stable Salary',
+    'coffee-addict-with-stable-salary',
+    'Alice has a good job, and even healthier coffee addiction!',
+    [stableSalary, lotsOfCoffees]
+  ),
 ]
